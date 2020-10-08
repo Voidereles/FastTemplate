d687fbbd954fac0d252ad4397278e896f7a9b9f4
Maciej <maciej.zelazo@protonmail.com>
2020-10-08 21:33:50 +0200

    header made

---
+++ script.js
@@ -0,0 +1,11 @@
+$('.hero__carousel').owlCarousel({
+    loop: true,
+    lazyLoad: true,
+    margin: 0,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    items: 1,
+    nav: false,
+    dots: false,
+});
