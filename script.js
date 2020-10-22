56a3a1f89f618ca7d780eea2926f44b0864c653c
Maciej <maciej.zelazo@protonmail.com>
2020-10-22 19:22:35 +0200

    main page made

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ $('.hero__carousel').owlCarousel({
     nav: false,
     dots: false,
 });
+
+$('.gallery__carousel').owlCarousel({
+    loop: true,
+    lazyLoad: true,
+    margin: 40,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    items: 1,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1,
+            dots: true
+        },
+        600: {
+            items: 2,
+            dots: true
+        },
+        992: {
+            items: 3,
+            dots: false
+        }
+    }
+});
